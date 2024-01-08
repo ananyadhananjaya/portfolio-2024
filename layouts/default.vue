3b1a178795d4e865a3e4e2b04339b75c9b4051ea
AX <ananya.dhananjaya1998@gmail.com>
2024-01-08 16:18:14 +0530

    feat: base version of nav, about

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <header class="py-5 px-3 flex items-center justify-between nav-bg">
+    <div>
+      <NuxtLink to="/" class="nav-link p-2 px-4" exact>Ananya</NuxtLink>
+    </div>
+    <nav class="space-x-4">
+      <NuxtLink to="/about" class="nav-link p-2 px-4">About</NuxtLink>
+    </nav>
+  </header>
+
+    <div class="flex-1">
+      <slot />
+    </div>
+    <footer class="flex justify-center py-4 mt-auto">
+      <p>&copy; 2023 Ananya Dhananjaya. All Rights Reserved.</p>
+    </footer>
+  </div>
+</template>
+
+
+<style scoped>
+.nav-link {
+  border-radius: 24px;
+  transition: background-color 0.5s;
+}
+
+.nav-link.router-link-active {
+  background-color: #618264;
+  color: #fff;
+}
+
+.nav-bg {
+  background-color: #B0D9B1;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="text-center mt-20">
+    <div class="text-9xl font-bold mb-6 animate__animated animate__fadeInUp">
+      Hi, I'm AD
+    </div>
+    <div class="text-9xl font-bold mb-6 border-text animate__animated animate__fadeInUp">
+      Hi, I'm AD
+    </div>
+    <div class="text-9xl font-bold mb-6 animate__animated animate__fadeInUp">
+      Hi, I'm AD
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  /* Add this link to your HTML head for animate.css styles */
+  @keyframes fadeInUp {
+    0% {
+      opacity: 0;
+      transform: scaleY(0);
+      transform-origin: bottom;
+    }
+    100% {
+      opacity: 1;
+      transform: scaleY(1);
+      transform-origin: bottom;
+    }
+  }
+
+  .animate__animated {
+    animation-duration: 0.2s; /* Adjust the duration as needed */
+    animation-fill-mode: both;
+    animation-timing-function: ease-out; /* Use ease-out for a quicker appearance */
+  }
+
+  .animate__delay-1s {
+    animation-delay: 0.5s; /* Adjust the delay as needed */
+  }
+
+  .animate__fadeInUp {
+    animation-name: fadeInUp;
+  }
+
+  /* Style for text with only borders */
+  .border-text {
+    -webkit-text-stroke: 2px #618264;
+    color: transparent;
+  }
+</style>
